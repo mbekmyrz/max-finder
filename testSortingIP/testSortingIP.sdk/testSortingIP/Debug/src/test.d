d3644866d03c1817bd91176bf4b1ffac7414a141
Marat Bekmyrza <marat.bekmyrza@nu.edu.kz>
2020-03-05 00:33:49 +0600

    Initial Commit

---
+++ testSortingIP/testSortingIP.sdk/testSortingIP/Debug/src/test.d
@@ -0,0 +1,28 @@
+src/test.o src/test.o: ../src/test.c \
+ ../../testSortingIP_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../testSortingIP_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../testSortingIP_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../testSortingIP_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../testSortingIP_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../testSortingIP_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../testSortingIP_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../testSortingIP_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../testSortingIP_bsp/ps7_cortexa9_0/include/xparameters.h
+
+../../testSortingIP_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../testSortingIP_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../testSortingIP_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../testSortingIP_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../testSortingIP_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../testSortingIP_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../testSortingIP_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../testSortingIP_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../testSortingIP_bsp/ps7_cortexa9_0/include/xparameters.h:
